7e23825cf3a764bc0db769e9aa23fcb35e23323c
Lucas-Bruce <lucasmatheusbruce@gmail.com>
2021-11-12 10:30:57 -0300

    Primeiro commit init

---
+++ bruce-api/src/main/java/br/com/bruce/bruceapi/BruceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruce.bruceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruceApiApplication.class, args);
+	}
+
+}
---
+++ bruce-api/src/test/java/br/com/bruce/bruceapi/BruceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruce.bruceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
